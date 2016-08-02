5241ad5e5d0e50fe4d8c15e4cb5969cd4f371d1c
赵菁 <peonie@163.com>
2016-08-02 14:25:03 +0800

    000

---
+++ resources/views/admin/device/classroom-view2.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+    <link href="/assets/css/system.css" rel="stylesheet">
+
+</head>
+
+<body>
+<main>
+    <div class="header" class="clearfix">
+        <h1>直播课堂</h1>
+    </div>
+    <div class="mainer" class="clearfix">
+        <div class="left">
+            <div class="video"></div>
+        </div>
+        <div class="right">
+            <ul class="rightbox">
+                <li>- 教室列表 -</li>
+                <li>教室1</li>
+                <li>教室1</li>
+                <li>教室1</li>
+                <li>教室1</li>
+                <li>教室1</li>
+                <li>教室1</li>
+                <li>教室1</li>
+                <li>教室1</li>
+                <li>教室1</li>
+                <li>教室1</li>
+                <li>教室1</li>
+                <li>教室1</li>
+                <li>教室1</li>
+                <li>教室1</li>
+
+
+
+            </ul>
+        </div>
+        <ul class="four">
+            <li>
+                <h3>PPT</h3>
+                <img src="/assets/images/li-img.png" width="192px" height="108px"/>
+            </li>
+            <li>
+                <h3>老师近景</h3>
+                <img src="/assets/images/li-img.png" width="192px" height="108px"/>
+            </li>
+            <li>
+                <h3>老师全景</h3>
+                <img src="/assets/images/li-img.png" width="192px" height="108px"/>
+            </li>
+            <li>
+                <h3>学生全景</h3>
+                <img src="/assets/images/li-img.png" width="192px" height="108px"/>
+            </li>
+            <li>
+                <h3>学生全景</h3>
+                <img src="/assets/images/li-img.png" width="192px" height="108px"/>
+            </li>
+            <li>
+                <h3>学生全景</h3>
+                <img src="/assets/images/li-img.png" width="192px" height="108px"/>
+            </li>
+        </ul>
+    </div>
+
+    <div class="footer">Powered by VideoHigh</div>
+</main>
+<script type="text/javascript" src="js/jquery-3.0.0.min.js"></script>
+<script>
+    $(function() {
+
+
+    })
+</script>
+</body>
+
+</html>

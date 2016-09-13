b80c146f7cfb474ceca7a5ead94ac25278e93d9b
duwenjun <252117159@qq.com>
2016-09-13 11:25:07 +0800

    test

--- app/Http/Routes/routes.task.php
+++ app/Http/Routes/routes.task.php
@@ -1,5 +1,6 @@
 <?php
 Route::group(['prefix' => 'api/task', 'namespace' => 'App\Http\Controllers\Api'], function () {
     Route::get('queue', 'TaskController@getQueue');
+    Route::get('queue2', 'TaskController@getQueue2');
     Route::get('callback', 'TaskController@getCallback');
 });

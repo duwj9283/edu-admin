b816fcd27cc06d04b3e66c8c3f48dad3d6e2206a
duwenjun <252117159@qq.com>
2016-09-21 10:01:46 +0800

    del task

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,6 +39,5 @@ class RouteServiceProvider extends ServiceProvider
             require app_path('Http/routes.php');
         });
         require app_path('Http/Routes/routes.news.php');
-        require app_path('Http/Routes/routes.task.php');
     }
 }

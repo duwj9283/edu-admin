5eb4a9a81e04a024062eec2cbb5e8df5fc59ef19
duwenjun <252117159@qq.com>
2016-09-21 10:26:07 +0800

    doc trans

--- app/Http/Controllers/Api/DocController.php
+++ app/Http/Controllers/Api/DocController.php
@@ -14,8 +14,31 @@ class DocController extends Controller
     {
         $id = intval($request->input('id'));
         $query = File::where('id', $id);
-        $row = $query->first();
-        print_r($row->toArray());
-        exit;
+        $file = $query->first();
+        $filePath = '/home/debian/www/upload/previewpool/'.$file->uid.'/'.$file->id.'/List.txt';
+        if(!file_exists($filePath))
+        {
+            $responseData = array("code" => 1, "msg" => "文档还未转换完成", "line" => __LINE__);
+            return $this->response($responseData);
+        }
+        if($fp=fopen($filePath,"a+"))
+        {
+            $conn=fread($fp,filesize($filePath));
+            $images = explode('|',strstr($conn,"1.jpg"));
+            $arr = array();
+            foreach($images as $image)
+            {
+                $basename = strstr($image,".jpg",true);
+                array_push($arr,'/api/source/getPreviewImage/'.$file->id.'/'.$basename);
+            }
+            $data['total'] = count($images);
+            $data['images'] = $arr;
+            $responseData = array("code"=>0,"msg"=>"","line"=>__LINE__,"data"=>$data);
+        }
+        else
+        {
+            $responseData = array("code" => 1, "msg" => "文件打不开", "line" => __LINE__);
+        }
+        return $this->response($responseData);
     }
 }
